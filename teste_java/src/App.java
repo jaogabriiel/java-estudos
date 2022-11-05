efd976402983ba791a59bee7731f54b9abd1566e
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2022-11-04 21:56:35 -0300

    Initial commit

---
+++ teste_java/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int numeroComparado = 18;
+        if (numeroComparado == 15) {
+            System.out.println("Os números são iguais");
+        } else {
+            System.out.println("Os números são diferentes");
+        }
+    }
+}
