4dfc1fb449068fb6a336ef3885376e89ea2a06cd
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2022-11-04 22:12:36 -0300

    testando variáveis

--- testando_variaveis/src/App.java
+++ testando_variaveis/src/App.java
@@ -1,5 +1,25 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numeroComparado = 18;
+        if (numeroComparado == 15) {
+            System.out.println("Os números são iguais");
+        } else {
+            System.out.println("Os números são diferentes");
+        }
+        if (numeroComparado != 21) {
+            System.out.println("Os números são diferentes");
+        } else {
+            System.out.println("Os números são iguais");
+        }
+        if (numeroComparado < 33) {
+            System.out.println("33 é maior que 18");
+        } else {
+            System.out.println("O 33 é menor que 18");
+        }
+        if (numeroComparado > 19) {
+            System.out.println("18 é maior que 19");
+        } else {
+            System.out.println("19 é maior que 18");
+        }
     }
 }
