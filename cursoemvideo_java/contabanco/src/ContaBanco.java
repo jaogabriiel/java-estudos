69d00e4023f53b0636a2b69517fcc5bccdc294e7
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2022-11-06 18:33:47 -0300

    início da prática de conta do banco

---
+++ cursoemvideo_java/contabanco/src/ContaBanco.java
@@ -0,0 +1,52 @@
+public class ContaBanco {
+    public int numConta;
+    protected String tipo;
+    private String dono;
+    private float saldo;
+    private boolean status;
+
+    public ContaBanco() {
+        this.saldo = 0;
+        status = false;
+    }
+
+    public void abrirconta() {
+        setTipo(tipo);
+        setStatus(true);
+    }
+
+    public int getNumConta() {
+        return numConta;
+    }
+    public void setNumConta(int numConta) {
+        this.numConta = numConta;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDono() {
+        return dono;
+    }
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
