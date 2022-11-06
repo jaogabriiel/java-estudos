dd7e409528e1133dd014bfdd3bde8afb1fd5365c
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2022-11-06 10:24:22 -0300

    métodos getters, setters e construtor

---
+++ cursoemvideo_java/getter_setter/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Caneta c1 = new Caneta("Nic", "Amarelo", 0.4f);
+        c1.status();
+        Caneta c2 = new Caneta("KKK","Laranja", 1.5f);
+        c2.status();
+    }
+}
