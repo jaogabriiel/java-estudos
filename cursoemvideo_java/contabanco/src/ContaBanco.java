1892de9db789db0b6846d0ac7555b724f9ff6f82
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2022-11-06 21:04:23 -0300

    conta bancária em poo

--- cursoemvideo_java/contabanco/src/ContaBanco.java
+++ cursoemvideo_java/contabanco/src/ContaBanco.java
@@ -1,3 +1,4 @@
+
 public class ContaBanco {
     public int numConta;
     protected String tipo;
@@ -10,9 +11,59 @@ public class ContaBanco {
         status = false;
     }
 
-    public void abrirconta() {
+    public void abrirconta(String tipo) {
         setTipo(tipo);
         setStatus(true);
+        if (tipo == "CC") {
+            saldo = 50;
+        } else {
+            saldo = 150;
+        }
+    }
+
+    public void fecharConta() {
+        if (saldo > 0) {
+            System.out.println("Conta com dinheiro!");
+        } else if (saldo < 0) {
+            System.out.println("Conta em débito");
+        } else {
+            setStatus(false);
+        }
+    }
+
+    public void depositar(float v) {
+        if (status = true) {
+            setSaldo(getSaldo() + v);
+        } else {
+            System.out.println("Impossível depositar");
+        }
+    }
+
+    public void sacar(float v) {
+        if (status = true) {
+            if (saldo > v){
+                saldo = saldo - v;
+            } else {
+                System.out.println("Saldo insuficiente");
+            }
+        } else {
+            System.out.println("Impossível sacar");
+        }
+    }
+
+    public void pagarMensal(float v) {
+        if (tipo == "CC") {
+            v = 12;
+        } else if (tipo == "CP") {
+            v = 20;
+        }
+        if (status = true) {
+            if (saldo > v) {
+                saldo = saldo - v;
+            } else {
+                System.out.println("Impossível pagar");
+            }
+        }
     }
 
     public int getNumConta() {
