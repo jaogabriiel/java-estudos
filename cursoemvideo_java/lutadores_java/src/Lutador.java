126b339ab1d851c1beeaafcc5c48be92a0ff8803
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2022-11-09 21:13:12 -0300

    relações de classes

---
+++ cursoemvideo_java/lutadores_java/src/Lutador.java
@@ -0,0 +1,127 @@
+public class Lutador {
+    private String nome;
+    private String nacionalidade;
+    private int idade;
+    private float altura;
+    private float peso;
+    private String categoria;
+    private int vitorias, derrotas, empates;
+
+    public void apresentar() {
+        System.out.println("--------------------------------------------------------");
+        System.out.println("CHEGOU A HORA! Apresentamos o lutador " + this.getNome());
+        System.out.println("Diretamente de " + this.getNacionalidade());
+        System.out.println("Com " + this.getIdade() + "anos e " + this.getAltura());
+        System.out.println("Pesando " + this.getPeso() + "Kg");
+        System.out.println(this.getVitorias() + " vitórias");
+        System.out.println(this.getDerrotas() + " derrotas e");
+        System.out.println(this.getEmpates() + " empates");
+    }
+
+    public void status() {
+        System.out.println(this.getNome() + " é um peso " + this.getCategoria());
+        System.out.println("Ganhou " + this.getVitorias() + " vezes");
+        System.out.println("Perdeu " + this.getDerrotas() + " vezes");
+        System.out.println("Empatou " + this.getEmpates() + " vezes");
+    }
+
+    public void ganharLuta() {
+        this.setVitorias(this.getVitorias() + 1);
+        // this.vitorias = this.vitorias + 1;
+    }
+
+    public void perderLuta() {
+        this.setDerrotas(this.getDerrotas() + 1);
+    }
+
+    public void empatarluta() {
+        this.setEmpates(this.getEmpates() + 1);
+    }
+
+    public Lutador(String nome, String nacionalidade, int idade, float altura, float peso, int vitorias, int derrotas, int empates) {
+        this.nome = nome;
+        this.nacionalidade = nacionalidade;
+        this.idade = idade;
+        this.altura = altura;
+        this.setPeso(peso);
+        this.vitorias = vitorias;
+        this.derrotas = derrotas;
+        this.empates = empates;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNacionalidade() {
+        return this.nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+    public int getIdade() {
+        return this.idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public float getAltura() {
+        return this.altura;
+    }
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float getPeso() {
+        return this.peso;
+    }
+    public void setPeso(float peso) {
+        this.peso = peso;
+        this.setCategoria();
+    }
+
+    public String getCategoria() {
+        return this.categoria;
+    }
+    private void setCategoria() {
+        if (this.peso < 52.2) { 
+            this.categoria = "Inválido";
+            } else if (this.peso <= 70.3) {
+                this.categoria = "Leve";
+            } else if (this.peso <= 83.9) {
+                this.categoria = "Médio";
+            } else if (this.peso <= 120.2) {
+                this.categoria = "Pesado";
+            } else {
+                this.categoria = "Inválido";
+            }
+        }
+    
+
+    public int getVitorias() {
+        return this.vitorias;
+    }
+    public void setVitorias(int vitorias) {
+        this.vitorias = vitorias;
+    }
+
+    public int getDerrotas() {
+        return this.derrotas;
+    }
+    public void setDerrotas(int derrotas) {
+        this.derrotas = derrotas;
+    }
+
+    public int getEmpates() {
+        return this.empates;
+    }
+    public void setEmpates(int empates) {
+        this.empates = empates;
+    }
+
+}
---
+++ cursoemvideo_java/lutadores_java/src/UltraEmojiCombat.java
@@ -0,0 +1,9 @@
+public class UltraEmojiCombat {
+    public static void main(String[] args) throws Exception {
+       Lutador l[] = new Lutador [6];
+       l[0] = new Lutador("Pretty boy", "França", 31, 1.75f, 68.9f, 11, 2, 1);
+       l [1] = new Lutador("Putscript", "Brasil", 29, 1.68f, 57.8f, 14, 2, 3);
+
+       l[1].apresentar();
+    }
+}
