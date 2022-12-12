06c338a2005e79ecaeb2664229dcac9988d75ec8
João Gabriel <115904431+jaogabriiel@users.noreply.github.com>
2022-12-12 20:49:27 -0300

    av1 - poo

--- trabalho_facul/src/Temperatura.java
+++ trabalho_facul/src/Temperatura.java
@@ -1,5 +1,35 @@
 public class Temperatura {
-    final int linha = 48;
-    final int coluna = 2;
-    int[][] numeros = new int[linha][coluna];
+    private float graus;
+    private String horario;
+
+    private float[][] dados_temp;
+
+    public void Temperatura() {
+        float[][] dados_temp = new float[48][2];
+    }
+
+   public float getGraus() {
+       return this.graus;
+   }
+   public void setGraus(float graus) {
+       this.graus = graus;
+   }
+
+    public String getHorario() {
+        return this.horario;
+    }
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public void dados(float horario, float grau) {
+        dados_temp[0][0] = 00;
+        dados_temp[0][1] = 25;
+        dados_temp[4][0] = 02;
+        dados_temp[4][1] = 29;
+        dados_temp[17][0] = 93;
+        dados_temp[17][1] = 37;
+        dados_temp[31][0] = 18;
+        dados_temp[31][1] = 26; 
+    }
 }
--- trabalho_facul/src/Umidade.java
+++ trabalho_facul/src/Umidade.java
@@ -1,3 +1,28 @@
 public class Umidade {
-    
+    private float procentagem;
+    private int hora;
+
+    private int[][] dados_um;
+
+    public Umidade() {
+        //int dados_um = new int[48][2];
+    }
+
+    public float getProcentagem() {
+        return this.procentagem;
+    }
+    public void setProcentagem(float procentagem) {
+        this.procentagem = procentagem;
+    }
+
+    public int getHora() {
+        return this.hora;
+    }
+    public void setHora(int hora) {
+        this.hora = hora;
+    }
+
+    public void dados() {
+
+    }
 }
